a5cef338636bc9c0960047a909c23b5bd222edf2
Erik van Zijst <erik.van.zijst@gmail.com>
2021-05-17 00:43:47 -0700

    Halved the sin/cos precision
    This freed enough die space to fit on 300x300um.

--- trigluts.py
+++ trigluts.py
@@ -1,16 +1,14 @@
 #!/usr/bin/env python3
+#
+# Generates a quarter sine lookup rom.
 
-from math import radians, sin, cos
+from math import radians, sin
 
-step = radians(360) / 64
+step = radians(360) / 32
 
 with open('sine.lut', 'w') as f_sin:
-    with open('cosine.lut', 'w') as f_cos:
-        for i in range(64):
-            angle = i * step
+    for i in range(8+1):
+        angle = i * step
 
-            f_sin.write(bin(int(sin(angle)*127) & 0b11111111)[2:].rjust(8, '0'))
-            f_sin.write('\n')
-
-            f_cos.write(bin(int(cos(angle)*127) & 0b11111111)[2:].rjust(8, '0'))
-            f_cos.write('\n')
+        f_sin.write(bin(int(sin(angle)*127) & 0b11111111)[2:].rjust(8, '0'))
+        f_sin.write('\n')
