76353a2bb1550ba32ef30d73f40e332ce7955b14
Erik van Zijst <erik.van.zijst@gmail.com>
2021-05-29 23:08:15 -0700

    Render Pong on VGA

--- src/vga.v
+++ src/vga.v
@@ -28,25 +28,24 @@ module vga (
         bgcounter <= 0;
     end
 
-    assign x_px_scaled = x_px >> 4;
-    assign y_px_scaled = y_px >> 4;
+    assign x_px_scaled = x_px[7:4];
+    assign y_px_scaled = y_px[7:4];
 //    assign background = ((x_px ^ y_px) % 10'd9) == 1 ? {bgcolor[2], bgcolor[2], bgcolor[1], bgcolor[1], bgcolor[0], bgcolor[0]} : 6'b000000;
     assign background = x_px[8:3];
-    assign rrggbb = activevideo ? background : 6'b000000;
 
-//    assign rrggbb = activevideo && (
-//        (x_px_scaled == ball_x && y_px_scaled == ball_y) ||
-//        (x_px_scaled == 4'b0 && lpaddle[y_px_scaled]) ||
-//        (x_px_scaled == 4'hf && rpaddle[y_px_scaled])
-//    ) ?
-//    6'b101010 : background;
+    assign rrggbb = activevideo ? (x_px[9:8] == 2'b00 && y_px[9:8] == 2'b00 && (
+        (x_px_scaled == ball_x && y_px_scaled == ball_y) ||
+        (x_px_scaled == 4'b0 && lpaddle[y_px_scaled]) ||
+        (x_px_scaled == 4'hf && rpaddle[y_px_scaled])
+    ) ? 6'b111111 : {bgcolor[2], 1'b0, bgcolor[1], 1'b0, bgcolor[0], 1'b0}) :
+    6'b000000;
 
     vgasync vga_0 (.px_clk(clk), .hsync(hsync), .vsync(vsync), .x_px(x_px), .y_px(y_px), .activevideo(activevideo), .reset(reset));
 
     always @(posedge clk) begin
         bgcounter <= bgcounter + 1;
         if (bgcounter == 0) begin
-            bgcolor = bgcolor == 3'b111 ? 1 : bgcolor + 1;
+            bgcolor <= bgcolor + 1;
         end
     end
 
